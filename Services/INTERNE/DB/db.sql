ccd8762d85223b31a971fa4dff84cefe9435b6a5
indigoua2 <80966619+indigoua2@users.noreply.github.com>
2021-04-18 19:04:31 +0200

    Suppression d'un doublon "USE db_wt;"

--- Services/INTERNE/DB/db.sql
+++ Services/INTERNE/DB/db.sql
@@ -2,26 +2,27 @@ CREATE USER 'wtl2-1'@'localhost' IDENTIFIED BY 'vps-ikram';
 CREATE USER 'wtl2-1'@'172.16.0.1' IDENTIFIED BY 'vps-ikram';
 CREATE USER 'wtl2-1'@'51.210.44.174' IDENTIFIED BY 'vps-ikram';
 CREATE USER 'wtl2-1'@'172.16.0.3' IDENTIFIED BY 'vps-ikram';
+CREATE USER 'wtl2-1'@'135.125.101.200' IDENTIFIED by 'vps-ikram';
+
 CREATE DATABASE db_wt;
 
-GRANT ALL PRIVILEGES ON *.*  TO 'wtl2-1'@'172.16.0.1';
-GRANT ALL PRIVILEGES ON *.*  TO 'wtl2-1'@'localhost';
-GRANT ALL PRIVILEGES ON *.*  TO 'wtl2-1'@'51.210.44.174';
-GRANT ALL PRIVILEGES ON *.*  TO 'wtl2-1'@'172.16.0.3';
+GRANT ALL PRIVILEGES ON db_wt.*  TO 'wtl2-1'@'172.16.0.1';
+GRANT ALL PRIVILEGES ON db_wt.*  TO 'wtl2-1'@'localhost';
+GRANT ALL PRIVILEGES ON db_wt.*  TO 'wtl2-1'@'51.210.44.174';
+GRANT ALL PRIVILEGES ON db_wt.*  TO 'wtl2-1'@'172.16.0.3';
+GRANT ALL PRIVILEGES ON db_wt.*  TO 'wtl2-1'@'135.125.101.200';
+
 USE db_wt;
 
 CREATE TABLE jeux(
-	id_jeu INTEGER NOT NULL AUTO_INCREMENT,
-	nom_jeu CHAR(40) NOT NULL,
-	prix DECIMAL(5,2) NOT NULL,
-	PRIMARY KEY(id_jeu)
-	
+    id_jeu INTEGER NOT NULL AUTO_INCREMENT,
+    nom_jeu CHAR(40) NOT NULL,
+    prix DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY(id_jeu)
+
 );
 
 INSERT INTO jeux (nom_jeu, prix)
 VALUES ("cheval en bois", 32.50),
        ("guitarre", 43.00),
-       ("arc à flèches", 9.99);
-
-
-USE db_wt;
+       ("arc a fleches", 9.99);
