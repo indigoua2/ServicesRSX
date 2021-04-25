41a48ded82a8fd938ac3bc638bc149ca94e307f1
indigoua2 <80966619+indigoua2@users.noreply.github.com>
2021-04-25 09:05:28 +0200

    Ajout "+ le port si nécessaire"

--- Services/INTERNE/ERP/index.php
+++ Services/INTERNE/ERP/index.php
@@ -1,6 +1,6 @@
 <?php
 	$connexion = mysqli_connect('ip', 'username', 'password', 'db_wt') or die('Erreur de connexion');
-	#Précise l'ip sur laquelle aller chercher la DB, le nom de l'utilisateur qui possède des permissions ainsi que son mot de passe, et le nom de la DB
+	#Précise l'ip sur laquelle aller chercher la DB + le port si nécessaire, le nom de l'utilisateur qui possède des permissions ainsi que son mot de passe, et le nom de la DB
 	#Par soucis de sécurité, les informations confidentielles ne sont pas divulguées ici, juste la structure utilisée
 ?>
 <html>
